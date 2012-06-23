462a97e094d80c9abe14689334ae6770a8f8507d
liuning108 <122273014@qq.com>
2012-06-23 10:14:04 +0800

    Update master

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
         config.div ='<div id="zoom_clipboard_314159265" style="cursor:pointer;background-color:rgb(173,255,254);border:1px solid;border-color:#92CDCC;text-transform:none;visibility:visible;white-space:normal;word-break:normal;word-spacing:normal;z-index:999999;border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;box-shadow:none;-webkit-box-shadow:none;background-attachment:scroll;text-align:left;text-indent:0px;widows:2;opacity:1!important;display:block!important;border-top-width:10px!important;border-bottom-width:10px!important;border-right-width:10px!important;border-left-width:10px!important;position:absolute!important;left:0px;top:0px;overflow-x:hidden!important;overflow-y:hidden!important;width:1306px;height:226px;"></div>';
        
             config.main=function(){
-              
+                var cloneStyle= function(o){};
                   
                 $('body').append(config.div);
                 $('#zoom_clipboard_314159265').hide();
@@ -12,7 +12,7 @@
                     if ($(this).attr('id')=='zoom_clipboard_314159265')return;
                     if (t_width>500 && t_heigth>600) return;
                     if ($(this).html().length<=0)return; 
-                      var  cloneStyle= function(o){alert(o);};
+                   
                       $('#zoom_clipboard_314159265').empty();
                       $('#zoom_clipboard_314159265').show();
                       $('#zoom_clipboard_314159265').stop(true,true);
